784b2a80e26d82323f28281862459b5cc6b9711a
Daoquangthiet <thietdq1711@gmail.com>
2023-01-10 00:26:22 +0700

    update

--- components/CheckoutForm.js
+++ components/CheckoutForm.js
@@ -1,4 +1,4 @@
-import { Box, Grid, MenuList, Typography } from "@mui/material";
+import { Box, FormControl, Grid, MenuList, Typography } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -10,12 +10,14 @@ import MenuItem from "@mui/material/MenuItem";
 import { makeStyles } from "@material-ui/core/styles";
 import Banner from "../assets/img/banner_page.png";
 import { AppContext } from "../libs/context/AppContext";
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import gql from "graphql-tag";
 import client from "../libs/apollo/ApolloClient";
 import { reach } from "yup";
 import { getFormattedCart } from "../function";
 import GET_CART from "../libs/queries/get-cart";
+import CHECKOUT_MUTATION from "../libs/mutations/checkout";
+import CLEAR_CART_MUTATION from "../libs/mutations/clear-cart";
 
 const useStyles = makeStyles({
   page: {
@@ -180,131 +182,208 @@ const CheckoutForm = (props) => {
     onCompleted: (data) => {
       // Update cart in the localStorage.
       const updatedCart = getFormattedCart(data);
-      if (typeof window !== "undefined") {
-        localStorage.setItem("woo-next-cart", JSON.stringify(updatedCart));
-        setCart(updatedCart);
-      }
+      console.log(updatedCart);
+      // if (typeof window !== "undefined") {
+      //   localStorage.setItem("woo-next-cart", JSON.stringify(updatedCart));
+      //   setCart(updatedCart);
+      // }
 
       // Update cart data in React Context.
     },
   });
 
+  const [checkout, { data: checkoutResponse, loading: checkoutLoading }] =
+    useMutation(CHECKOUT_MUTATION, {
+      variables: {
+        input: orderData,
+      },
+      onError: (error) => {
+        if (error) {
+          setRequestError(error?.graphQLErrors?.[0]?.message ?? "");
+        }
+      },
+    });
+  const [clearCartMutation] = useMutation(CLEAR_CART_MUTATION);
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    /**
+     * Validate Billing and Shipping Details
+     *
+     * Note:
+     * 1. If billing is different than shipping address, only then validate billing.
+     * 2. We are passing theBillingStates?.length and theShippingStates?.length, so that
+     * the respective states should only be mandatory, if a country has states.
+     */
+    const billingValidationResult = input?.billingDifferentThanShipping
+      ? validateAndSanitizeCheckoutForm(
+          input?.billing,
+          theBillingStates?.length
+        )
+      : { errors: null, isValid: true };
+    const shippingValidationResult = validateAndSanitizeCheckoutForm(
+      input?.shipping,
+      theShippingStates?.length
+    );
+
+    if (!shippingValidationResult.isValid || !billingValidationResult.isValid) {
+      setInput({
+        ...input,
+        billing: { ...input.billing, errors: billingValidationResult.errors },
+        shipping: {
+          ...input.shipping,
+          errors: shippingValidationResult.errors,
+        },
+      });
+
+      return;
+    }
+
+    if ("stripe-mode" === input.paymentMethod) {
+      const createdOrderData = await handleStripeCheckout(
+        input,
+        cart?.products,
+        setRequestError,
+        clearCartMutation,
+        setIsStripeOrderProcessing,
+        setCreatedOrderData
+      );
+      return null;
+    }
+
+    const checkOutData = createCheckoutData(input);
+    setRequestError(null);
+    /**
+     *  When order data is set, checkout mutation will automatically be called,
+     *  because 'orderData' is added in useEffect as a dependency.
+     */
+    setOrderData(checkOutData);
+  };
+
   return (
     <>
-      <Grid iteam lg={8}>
-        <Typography variant="h3">Checkout Page</Typography>
-        <Paper className={classes.contactNumber}>
-          <MenuList>
-            <MenuItem sx={{ marginBottom: "10px" }}>
-              <ListItemText>
-                <Typography
-                  variant="div"
-                  color="#fff"
-                  className={classes.number}
-                >
-                  1
-                </Typography>
-                Contact Number
-              </ListItemText>
-              <ListItemIcon sx={{ color: "#40c6ff" }}>
-                <AddIcon label="add" fontSize="small" />
-                Update
-              </ListItemIcon>
-            </MenuItem>
-          </MenuList>
-          <Box sx={{ display: "flex", padding: "15px" }}>
-            <Box className={classes.boxinput}>
-              <input className={classes.formInput} type="text" />
-            </Box>
-            <Box className={classes.boxinput}>
-              <input className={classes.formInput} type="text" />
-            </Box>
-          </Box>
-        </Paper>
-        <Paper className={classes.billingAddress}>
-          <MenuList>
-            <MenuItem sx={{ marginBottom: "10px" }}>
-              <ListItemText>
-                <Typography
-                  variant="div"
-                  color="#fff"
-                  className={classes.number}
-                >
-                  2
-                </Typography>
-                Billing Address
-              </ListItemText>
-              <ListItemIcon sx={{ color: "#40c6ff" }}>
-                <AddIcon label="add" fontSize="small" />
-                Update
-              </ListItemIcon>
-            </MenuItem>
-          </MenuList>
-          <Box sx={{ display: "flex", padding: "15px" }}>
-            <Box className={classes.boxinput}>
-              <input className={classes.formInput} type="text" />
-            </Box>
-            <Box className={classes.boxinput}>
-              <input className={classes.formInput} type="text" />
-            </Box>
-          </Box>
-        </Paper>
-        <Paper className={classes.shippingAddress}>
-          <MenuList>
-            <MenuItem sx={{ marginBottom: "10px" }}>
-              <ListItemText>
-                <Typography
-                  variant="div"
-                  color="#fff"
-                  className={classes.number}
-                >
-                  3
-                </Typography>
-                Shipping Address
-              </ListItemText>
-              <ListItemIcon sx={{ color: "#40c6ff" }}>
-                <AddIcon label="add" fontSize="small" />
-                Update
-              </ListItemIcon>
-            </MenuItem>
-          </MenuList>
-          <Box sx={{ display: "flex", padding: "15px" }}>
-            <Box className={classes.boxinput}>
-              <input className={classes.formInput} type="text" />
-            </Box>
-            <Box className={classes.boxinput}>
-              <input className={classes.formInput} type="text" />
-            </Box>
-          </Box>
-        </Paper>
-        <Paper className={classes.deliverySchedule}>
-          <MenuList>
-            <MenuItem sx={{ marginBottom: "10px" }}>
-              <ListItemText>
-                <Typography
-                  variant="div"
-                  color="#fff"
-                  className={classes.number}
-                >
-                  4
-                </Typography>
-                Delivery Schedule
-              </ListItemText>
-            </MenuItem>
-          </MenuList>
-          <Box sx={{ display: "flex", padding: "15px" }}>
-            <Box className={classes.boxinput}>
-              <input className={classes.formInput} type="text" />
-            </Box>
-            <Box className={classes.boxinput}>
-              <input className={classes.formInput} type="text" />
-            </Box>
-            <Box className={classes.boxinput}>
-              <input className={classes.formInput} type="text" />
-            </Box>
-          </Box>
-        </Paper>
-      </Grid>
+      {cart ? (
+        <FormControl
+          onSubmit={handleFormSubmit}
+          className={classes.checkoutForm}
+        >
+          <Grid iteam className={classes.containerForm} >
+            <Typography variant="h3">Checkout Page</Typography>
+            <Paper className={classes.contactNumber}>
+              <Typography variant="h4">Billing Details</Typography>
+              <MenuList>
+                <MenuItem sx={{ marginBottom: "10px" }}>
+                  <ListItemText>
+                    <Typography
+                      variant="div"
+                      color="#fff"
+                      className={classes.number}
+                    >
+                      1
+                    </Typography>
+                    Contact Number
+                  </ListItemText>
+                  <ListItemIcon sx={{ color: "#40c6ff" }}>
+                    <AddIcon label="add" fontSize="small" />
+                    Update
+                  </ListItemIcon>
+                </MenuItem>
+              </MenuList>
+              <Box sx={{ display: "flex", padding: "15px" }}>
+                <Box className={classes.boxinput}>
+                  <input className={classes.formInput} type="text" />
+                </Box>
+                <Box className={classes.boxinput}>
+                  <input className={classes.formInput} type="text" />
+                </Box>
+              </Box>
+            </Paper>
+            <Paper className={classes.billingAddress}>
+              <MenuList>
+                <MenuItem sx={{ marginBottom: "10px" }}>
+                  <ListItemText>
+                    <Typography
+                      variant="div"
+                      color="#fff"
+                      className={classes.number}
+                    >
+                      2
+                    </Typography>
+                    Billing Address
+                  </ListItemText>
+                  <ListItemIcon sx={{ color: "#40c6ff" }}>
+                    <AddIcon label="add" fontSize="small" />
+                    Update
+                  </ListItemIcon>
+                </MenuItem>
+              </MenuList>
+              <Box sx={{ display: "flex", padding: "15px" }}>
+                <Box className={classes.boxinput}>
+                  <input className={classes.formInput} type="text" />
+                </Box>
+                <Box className={classes.boxinput}>
+                  <input className={classes.formInput} type="text" />
+                </Box>
+              </Box>
+            </Paper>
+            <Paper className={classes.shippingAddress}>
+              <MenuList>
+                <MenuItem sx={{ marginBottom: "10px" }}>
+                  <ListItemText>
+                    <Typography
+                      variant="div"
+                      color="#fff"
+                      className={classes.number}
+                    >
+                      3
+                    </Typography>
+                    Shipping Address
+                  </ListItemText>
+                  <ListItemIcon sx={{ color: "#40c6ff" }}>
+                    <AddIcon label="add" fontSize="small" />
+                    Update
+                  </ListItemIcon>
+                </MenuItem>
+              </MenuList>
+              <Box sx={{ display: "flex", padding: "15px" }}>
+                <Box className={classes.boxinput}>
+                  <input className={classes.formInput} type="text" />
+                </Box>
+                <Box className={classes.boxinput}>
+                  <input className={classes.formInput} type="text" />
+                </Box>
+              </Box>
+            </Paper>
+            <Paper className={classes.deliverySchedule}>
+              <MenuList>
+                <MenuItem sx={{ marginBottom: "10px" }}>
+                  <ListItemText>
+                    <Typography
+                      variant="div"
+                      color="#fff"
+                      className={classes.number}
+                    >
+                      4
+                    </Typography>
+                    Delivery Schedule
+                  </ListItemText>
+                </MenuItem>
+              </MenuList>
+              <Box sx={{ display: "flex", padding: "15px" }}>
+                <Box className={classes.boxinput}>
+                  <input className={classes.formInput} type="text" />
+                </Box>
+                <Box className={classes.boxinput}>
+                  <input className={classes.formInput} type="text" />
+                </Box>
+                <Box className={classes.boxinput}>
+                  <input className={classes.formInput} type="text" />
+                </Box>
+              </Box>
+            </Paper>
+          </Grid>
+        </FormControl>
+      ) : null}
     </>
   );
 };
