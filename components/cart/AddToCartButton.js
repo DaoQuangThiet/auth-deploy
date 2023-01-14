20852f9a1412404c9237129cf43b6dd70028e052
DaoQuangThiet <Thietdq1711@gmail.com>
2023-01-14 17:00:07 +0700

    update

--- components/cart/AddToCartButton.js
+++ components/cart/AddToCartButton.js
@@ -1,85 +1,126 @@
-import Link from 'next/link';
-import React from 'react';
-import { useState, useContext } from 'react';
-import { AppContext } from '../../libs/context/AppContext';
-import { addFirstProduct, updateCart } from '../../function';
+import Link from "next/link";
+import React from "react";
+import { useState, useContext } from "react";
+import { AppContext } from "../../libs/context/AppContext";
+import { addFirstProduct, getFormattedCart, updateCart } from "../../function";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { AppProvider } from '../../libs/context/AppContext';
+import { AppProvider } from "../../libs/context/AppContext";
+import { v4 } from "uuid";
+import { useMutation, useQuery } from "@apollo/client";
+import GET_CART from "../../libs/queries/get-cart";
+import ADD_TO_CART from "../../libs/mutations/add-to-cart";
 
-const colorHover = '#40c6ff';
+const colorHover = "#40c6ff";
 
 const useStyles = makeStyles({
-    btnviewcart: {
-        margin: '25px',
+  btnviewcart: {
+    margin: "25px",
+  },
+  AddToCartButton: {
+    borderRadius: "20px",
+    backgroundColor: `${colorHover}`,
+    height: "40px",
+    lineHeight: "34px",
+    padding: "0px 30px",
+    color: "#fff",
+    border: "none",
+    textTransform: "uppercase",
+    fontSize: "16px",
+    marginTop: "20px",
+    "&:hover": {
+      cursor: "pointer",
     },
-    AddToCartButton: {
-        borderRadius: '20px',
-        backgroundColor: `${colorHover}`,
-        height: '40px',
-        lineHeight: '34px',
-        padding: '0px 30px',
-        color: '#fff',
-        border: 'none',
-        textTransform: 'uppercase',
-        fontSize: '16px',
-        marginTop: '20px',
-        '&:hover': {
-            cursor: 'pointer',
-        },
-        '&:last-child': {
-            marginLeft: '20px',
-        }
-    }
+    "&:last-child": {
+      marginLeft: "20px",
+    },
+  },
 });
 
-
 const AddToCartButton = (props) => {
+  const classes = useStyles();
+  const { product } = props;
+  console.log(product);
+  const [cart, setCart] = useContext(AppContext);
+  const [showViewCart, setShowViewCart] = useState(false);
+  const [requestError, setRequestError] = useState(null);
+  const productQryInput = {
+    clientMutationId: v4(),
+    productId: product.productId,
+  };
 
-    const classes = useStyles();
-    const { product } = props;
-    const [cart, setCart] = useContext(AppContext);
-    const [showViewCart, setShowViewCart] = useState(false);
+  const { data, refetch } = useQuery(GET_CART, {
+    notifyOnNetworkStatusChange: true,
+    onCompleted: () => {
+      const updatedCart = getFormattedCart(data);
+      localStorage.setItem("woo-next-cart", JSON.stringify(updatedCart));
 
+      setCart(updatedCart);
+    },
+  });
+  const [
+    addToCart,
+    { data: addToCartRes, loading: addToCartLoading, error: addToCartError },
+  ] = useMutation(ADD_TO_CART, {
+    variables: {
+      input: productQryInput,
+    },
+    onCompleted: () => {
+      refetch();
+      setShowViewCart(true);
+    },
+    onError: (error) => {
+      if (error) {
+        setRequestError(error.graphQLErrors?.[0]?.message ?? "");
+      }
+    },
+  });
 
-    const handleAddToCartClick = () => {
-        if (process.browser) {
-            let existingCart = localStorage.getItem('woo-next-cart');
+  const handleAddToCartClick = async () => {
+    setRequestError(null);
+    await addToCart();
+    // if (process.browser) {
+    //   let existingCart = localStorage.getItem("woo-next-cart");
 
-            // if cart has item(s) already, then update the exiting
+    //   // if cart has item(s) already, then update the exiting
 
-            if (existingCart) {
-                existingCart = JSON.parse(existingCart);
+    //   if (existingCart) {
+    //     existingCart = JSON.parse(existingCart);
 
-                const qtyToBeAdded = 1;
+    //     const qtyToBeAdded = 1;
 
-                const updatedCart = updateCart(existingCart, product, qtyToBeAdded);
-                setCart(updatedCart);
+    //     const updatedCart = updateCart(existingCart, product, qtyToBeAdded);
+    //     setCart(updatedCart);
+    //   } else {
+    //     // add new cart
+    //     const newCart = addFirstProduct(product);
+    //     setCart(newCart);
+    //   }
+    //   setShowViewCart(true);
+    // }
+  };
 
-            } else {
-                // add new cart
-                const newCart = addFirstProduct(product);
-                setCart(newCart);
-            }
-            setShowViewCart(true);
-        }
-    };
-
-
-    return (
-        <AppProvider>
-            <React.Fragment>
-                <button className={classes.AddToCartButton} variant="contained" onClick={handleAddToCartClick} >Add To Cart</button>
-                {showViewCart ? (
-                    <Link href="/cart">
-                        <a>
-                            <button variant="contained" className={classes.AddToCartButton}>View Cart</button>
-                        </a>
-                    </Link>
-                ) : ''}
-            </React.Fragment>
-        </AppProvider>
-    )
+  return (
+    <AppProvider>
+      <React.Fragment>
+        <button
+          className={classes.AddToCartButton}
+          onClick={handleAddToCartClick}
+        >
+          {addToCartLoading ? "Adding To Cart..." : "Add To Cart"}
+        </button>
+        {showViewCart ? (
+          <Link href="/cart">
+            <a>
+              <button className={classes.AddToCartButton}>View Cart</button>
+            </a>
+          </Link>
+        ) : (
+          ""
+        )}
+      </React.Fragment>
+    </AppProvider>
+  );
 };
 
 export default AddToCartButton;
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -21,6 +21,8 @@ import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Banner from "../../assets/img/banner_page.png";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { AppProvider } from "../../libs/context/AppContext";
+import { ApolloProvider } from "@apollo/client";
 
 const colorHeading = "#323232";
 const colorHover = "#40c6ff";
@@ -205,155 +207,169 @@ export default function Product(props) {
 
   return (
     <>
-      <Box className={classes.page}>
-        <Container>
-          <Box className={classes.titlePage}>
-            <Grid item lg={6}>
-              <Box>
-                <Typography
-                  className={classes.textTile}
-                  component="h3"
-                  variant="h3"
-                >
-                  Product Detail
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item lg={6}>
-              <Box className={classes.rightTextPage}>
-                <Breadcrumbs sx={{ color: "white" }} aria-label="breadcrumb">
-                  <Typography
-                    className={classes.titleText}
-                    component="h6"
-                    variant="h6"
-                  >
-                    Home
-                  </Typography>
+      <AppProvider>
+        <ApolloProvider client={client}>
+          <Box className={classes.page}>
+            <Container>
+              <Box className={classes.titlePage}>
+                <Grid item lg={6}>
+                  <Box>
+                    <Typography
+                      className={classes.textTile}
+                      component="h3"
+                      variant="h3"
+                    >
+                      Product Detail
+                    </Typography>
+                  </Box>
+                </Grid>
+                <Grid item lg={6}>
+                  <Box className={classes.rightTextPage}>
+                    <Breadcrumbs
+                      sx={{ color: "white" }}
+                      aria-label="breadcrumb"
+                    >
+                      <Typography
+                        className={classes.titleText}
+                        component="h6"
+                        variant="h6"
+                      >
+                        Home
+                      </Typography>
 
-                  <Typography
-                    className={classes.titleText}
-                    component="h6"
-                    variant="h6"
-                  >
-                    Product Detail
-                  </Typography>
-                </Breadcrumbs>
+                      <Typography
+                        className={classes.titleText}
+                        component="h6"
+                        variant="h6"
+                      >
+                        Product Detail
+                      </Typography>
+                    </Breadcrumbs>
+                  </Box>
+                </Grid>
               </Box>
-            </Grid>
+            </Container>
           </Box>
-        </Container>
-      </Box>
 
-      <div>
-        {product ? (
-          <div className={classes.singleProduct}>
-            <Container maxWidth="lg">
-              <Grid container direction="row" lg={12}>
-                <Grid item xs={12} sm={12} md={6} lg={6}>
-                  <div className={classes.imgagesDetail}>
-                    <img src={product?.image?.sourceUrl} alt="" />
-                    <div className={classes.galleryImages}>
-                      {!isEmpty(product?.galleryImages?.nodes) ? (
-                        <GalleryCarousel
-                          gallery={product?.galleryImages?.nodes}
-                        />
-                      ) : !isEmpty(product.image) ? (
-                        <img
-                          src={product?.image?.sourceUrl}
-                          alt="Product Image"
-                          height="auto"
-                          srcSet={product?.image?.srcSet}
-                        />
-                      ) : null}
-                    </div>
-                  </div>
-                </Grid>
-                <Grid item xs={12} sm={12} md={6} lg={6}>
-                  <div className="descDetail">
-                    <h1 className={classes.name_product_detail}>
-                      {product.name}
-                    </h1>
-                    <Price
-                      salesPrice={product?.price}
-                      regularPrice={product?.regularPrice}
-                    />
-                    <div
-                      dangerouslySetInnerHTML={{
-                        __html: product.shortDescription,
-                      }}
-                      className={classes.description_shot}
-                    />
-                    {<AddToCartButton product={product} />}
-                    <div className={classes.product_meta}>
-                      <div className={classes.sku_title}>
-                        SKU: N/A {product.sku}
-                      </div>
-                      {!isEmpty(product?.productCategories?.nodes) ? (
-                        <CategoriesCarousel
-                          gallery={product?.productCategories?.nodes}
-                        />
-                      ) : !isEmpty(product.name) ? (
-                        <div className={classes.sku_name}>
-                          SKU: {product.name}
+          <div>
+            {product ? (
+              <div className={classes.singleProduct}>
+                <Container maxWidth="lg">
+                  <Grid container direction="row" lg={12}>
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                      <div className={classes.imgagesDetail}>
+                        <img src={product?.image?.sourceUrl} alt="" />
+                        <div className={classes.galleryImages}>
+                          {!isEmpty(product?.galleryImages?.nodes) ? (
+                            <GalleryCarousel
+                              gallery={product?.galleryImages?.nodes}
+                            />
+                          ) : !isEmpty(product.image) ? (
+                            <img
+                              src={product?.image?.sourceUrl}
+                              alt="Product Image"
+                              height="auto"
+                              srcSet={product?.image?.srcSet}
+                            />
+                          ) : null}
                         </div>
-                      ) : null}
+                      </div>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                      <div className="descDetail">
+                        <h1 className={classes.name_product_detail}>
+                          {product.name}
+                        </h1>
+                        <Price
+                          salesPrice={product?.price}
+                          regularPrice={product?.regularPrice}
+                        />
+                        <div
+                          dangerouslySetInnerHTML={{
+                            __html: product.shortDescription,
+                          }}
+                          className={classes.description_shot}
+                        />
+                        {<AddToCartButton product={product} />}
+                        <div className={classes.product_meta}>
+                          <div className={classes.sku_title}>
+                            SKU: N/A {product.sku}
+                          </div>
+                          {!isEmpty(product?.productCategories?.nodes) ? (
+                            <CategoriesCarousel
+                              gallery={product?.productCategories?.nodes}
+                            />
+                          ) : !isEmpty(product.name) ? (
+                            <div className={classes.sku_name}>
+                              SKU: {product.name}
+                            </div>
+                          ) : null}
+                        </div>
+                      </div>
+                    </Grid>
+                    <div className={classes.description_detail}>
+                      <h4>Detail</h4>
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: product.description,
+                        }}
+                        className={classes.description_item}
+                      ></div>
                     </div>
-                  </div>
-                </Grid>
-                <div className={classes.description_detail}>
-                  <h4>Detail</h4>
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: product.description,
-                    }}
-                    className={classes.description_item}
-                  ></div>
-                </div>
-                <div className={classes.box_related_products}>
-                  <h4>Related products</h4>
-                  {product.related.nodes.length
-                    ? product.related.nodes.map((item) => (
-                        <div className={classes.related_products} key={item}>
-                          <div className={classes.box_product}>
-                            <Link href={`/product/${item?.slug}`}>
-                              <a>
-                                <img
-                                  src={`${item?.image?.sourceUrl}?w=164&h=164&fit=crop&auto=format`}
-                                  //srcSet={`${item?.image?.srcSet}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                                  alt={item?.title}
-                                  loading="lazy"
-                                />
-                              </a>
-                            </Link>
-                            <div className={classes.cardBody}>
-                              <h3>
+                    <div className={classes.box_related_products}>
+                      <h4>Related products</h4>
+                      {product.related.nodes.length
+                        ? product.related.nodes.map((item) => (
+                            <div
+                              className={classes.related_products}
+                              key={item}
+                            >
+                              <div className={classes.box_product}>
                                 <Link href={`/product/${item?.slug}`}>
-                                  <a className={classes.cardTitle}>
-                                    {item.name}
+                                  <a>
+                                    <img
+                                      src={`${item?.image?.sourceUrl}?w=164&h=164&fit=crop&auto=format`}
+                                      //srcSet={`${item?.image?.srcSet}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                      alt={item?.title}
+                                      loading="lazy"
+                                    />
                                   </a>
                                 </Link>
-                              </h3>
-                              <div className={classes.startRating}>
-                                <Rating name="read-only" value="0" readOnly />
+                                <div className={classes.cardBody}>
+                                  <h3>
+                                    <Link href={`/product/${item?.slug}`}>
+                                      <a className={classes.cardTitle}>
+                                        {item.name}
+                                      </a>
+                                    </Link>
+                                  </h3>
+                                  <div className={classes.startRating}>
+                                    <Rating
+                                      name="read-only"
+                                      value="0"
+                                      readOnly
+                                    />
+                                  </div>
+                                  <h4 className={classes.cardPrice}>
+                                    <span className={classes.price_sales}>
+                                      {item.price}
+                                    </span>
+                                  </h4>
+                                </div>
                               </div>
-                              <h4 className={classes.cardPrice}>
-                                <span className={classes.price_sales}>
-                                  {item.price}
-                                </span>
-                              </h4>
                             </div>
-                          </div>
-                        </div>
-                      ))
-                    : ""}
-                </div>
-              </Grid>
-            </Container>
+                          ))
+                        : ""}
+                    </div>
+                  </Grid>
+                </Container>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
-        ) : (
-          ""
-        )}
-      </div>
+        </ApolloProvider>
+      </AppProvider>
     </>
   );
 }
