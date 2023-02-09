7ac097b38913dcfa568a5295bfc7a8c9a162a2ec
DaoQuangThiet <Thietdq1711@gmail.com>
2023-02-09 17:17:26 +0700

    Project Complete

--- components/AddHead.js
+++ components/AddHead.js
@@ -1,12 +1,19 @@
-import Head from 'next/head'
+import Head from "next/head";
 import React from "react";
 
 const AddHead = () => {
-    return (
-        <Head>
-            <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&display=swap" rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400&display=swap" rel="stylesheet" />
-        </Head>
-    )
-}
+  return (
+    <Head>
+      <title>Ebay | Next.js Theme</title>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+  );
+};
 export default AddHead;
--- components/checkout/Error.js
+++ components/checkout/Error.js
@@ -1,8 +1,8 @@
+import { Box } from "@mui/material";
+
 const Error = ({ errors, fieldName }) => {
   return errors && errors.hasOwnProperty(fieldName) ? (
-    <div className="invalid-feedback d-block text-red-500">
-      {errors[fieldName]}
-    </div>
+    <Box sx={{ color: "#f56565" }}>{errors[fieldName]}</Box>
   ) : (
     ""
   );
