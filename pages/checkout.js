8a9f4c4260d0ac5f9281f7041ad6b9076ae2824f
DaoQuangThiet <Thietdq1711@gmail.com>
2023-02-10 10:36:03 +0700

    Project Ebay-Theme + Next.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,45 +1,23 @@
-import * as React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container, Box, Grid } from "@mui/material";
-import MenuList from "@mui/material/MenuList";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemText from "@mui/material/ListItemText";
+import { Box, Container, Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
+import * as React from "react";
 
-import Divider from "@mui/material/Divider";
 // import Link from 'next/link';
-import { Link } from "@mui/material";
-import Checkbox from "@mui/material/Checkbox";
-import { Input } from "@material-ui/core";
 //import { TabsUnstyled , TabsListUnstyled , TabPanelUnstyled , TabUnstyled } from "@mui/base";
-import TabsUnstyled from "@mui/base/TabsUnstyled";
-import TabsListUnstyled from "@mui/base/TabsListUnstyled";
-import TabPanelUnstyled from "@mui/base/TabPanelUnstyled";
-import TabUnstyled from "@mui/base/TabUnstyled";
-import InputAdornment from "@mui/material/InputAdornment";
-import FormControl from "@mui/material/FormControl";
-import PaymentIcon from "@mui/icons-material/Payment";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal";
-import Banner from "../assets/img/banner_page.png";
+import { ApolloProvider } from "@apollo/client";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import { useRouter } from "next/router";
+import Banner from "../assets/img/banner_page.png";
 import CheckoutForm from "../components/CheckoutForm";
-import { AppProvider } from "../libs/context/AppContext";
-import gql from "graphql-tag";
 import client from "../libs/apollo/ApolloClient";
-import { ApolloProvider } from "@apollo/client";
-import GET_CART from "../libs/queries/get-cart";
-import GET_COUNTRIES from "../libs/queries/get-countries";
 
 const useStyles = makeStyles({
   page: {
     marginBottom: "60px",
     minHeight: "200px",
     backgroundImage: `url(${Banner.src})`,
-    backgroundSize: "cover",
+    backgroundSize: "cover"
   },
 
   titlePage: {
@@ -47,49 +25,49 @@ const useStyles = makeStyles({
     paddingTop: "75px",
     paddingBottom: "75px",
     "@media (max-width: 768px)": {
-      display: "block",
-    },
+      display: "block"
+    }
   },
 
   rightTextPage: {
     paddingTop: "12px",
     float: "right",
     "@media (max-width: 768px)": {
-      float: "inherit",
-    },
+      float: "inherit"
+    }
   },
 
   mainCheckout: {
     marginTop: "30px",
     display: "flex",
     "@media (max-width: 600px)": {
-      display: "block",
-    },
+      display: "block"
+    }
   },
   contactNumber: {
-    marginBottom: "35px",
+    marginBottom: "35px"
   },
   billingAddress: {
-    marginBottom: "35px",
+    marginBottom: "35px"
   },
   shippingAddress: {
-    marginBottom: "35px",
+    marginBottom: "35px"
   },
   delivery: {
     display: "flex",
     padding: "15px",
     "@media (max-width: 350px)": {
-      display: "block",
-    },
+      display: "block"
+    }
   },
   payment: {
     marginLeft: "20px",
     "@media (max-width: 768px)": {
-      marginLeft: "0px",
-    },
+      marginLeft: "0px"
+    }
   },
   tabListPay: {
-    marginBottom: "20px",
+    marginBottom: "20px"
   },
   tabPay: {
     border: "1px solid #40c6ff",
@@ -97,10 +75,10 @@ const useStyles = makeStyles({
     height: "50px",
     background: "white",
     margin: "10px",
-    cursor: "pointer",
+    cursor: "pointer"
   },
   tabPanePay: {
-    margin: "10px",
+    margin: "10px"
   },
   number: {
     border: "1px",
@@ -110,7 +88,7 @@ const useStyles = makeStyles({
     paddingRight: "7px",
     paddingLeft: "7px",
     paddingTop: "3px",
-    paddingBottom: "3px",
+    paddingBottom: "3px"
   },
   style: {
     position: "absolute",
@@ -121,33 +99,33 @@ const useStyles = makeStyles({
     background: "#fff",
     border: "2px solid #000",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   formInput: {
     width: "100%",
 
     height: 45,
     "&:focusVisible": {
-      outline: ["none"],
-    },
+      outline: ["none"]
+    }
   },
   boxinput: {
     width: "100%",
     paddingRight: "10px",
-    paddingLeft: "10px",
+    paddingLeft: "10px"
   },
   textTile: {
     color: "white",
     fontFamily: "Merriweather",
     fontSize: "50px",
-    fontWeight: 700,
+    fontWeight: 700
   },
   titleText: {
     color: "white",
     fontFamily: "Muli",
     fontWeight: 400,
-    fontSize: "14px",
-  },
+    fontSize: "14px"
+  }
 });
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
@@ -168,55 +146,50 @@ const Checkout = (props) => {
   // console.warn(existingCart);
   return (
     <ApolloProvider client={client}>
-      <AppProvider>
-        <Box className={classes.page}>
-          <Container>
-            <Box className={classes.titlePage}>
-              <Grid container spacing={1}>
-                <Grid item lg={6} md={6} xs={12}>
-                  <Box>
+      <Box className={classes.page}>
+        <Container>
+          <Box className={classes.titlePage}>
+            <Grid container spacing={1}>
+              <Grid item lg={6} md={6} xs={12}>
+                <Box>
+                  <Typography
+                    className={classes.textTile}
+                    component="h3"
+                    variant="h3"
+                  >
+                    Checkout
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item lg={6} md={6} xs={12}>
+                <Box className={classes.rightTextPage}>
+                  <Breadcrumbs sx={{ color: "white" }} aria-label="breadcrumb">
                     <Typography
-                      className={classes.textTile}
-                      component="h3"
-                      variant="h3"
+                      className={classes.titleText}
+                      component="h6"
+                      variant="h6"
+                    >
+                      Home
+                    </Typography>
+                    <Typography
+                      className={classes.titleText}
+                      component="h6"
+                      variant="h6"
                     >
                       Checkout
                     </Typography>
-                  </Box>
-                </Grid>
-                <Grid item lg={6} md={6} xs={12}>
-                  <Box className={classes.rightTextPage}>
-                    <Breadcrumbs
-                      sx={{ color: "white" }}
-                      aria-label="breadcrumb"
-                    >
-                      <Typography
-                        className={classes.titleText}
-                        component="h6"
-                        variant="h6"
-                      >
-                        Home
-                      </Typography>
-                      <Typography
-                        className={classes.titleText}
-                        component="h6"
-                        variant="h6"
-                      >
-                        Checkout
-                      </Typography>
-                    </Breadcrumbs>
-                  </Box>
-                </Grid>
+                  </Breadcrumbs>
+                </Box>
               </Grid>
-            </Box>
-          </Container>
-        </Box>
-        <Container>
-          <Box className={classes.mainCheckout}>
-            <CheckoutForm existingCart={existingCart} />
+            </Grid>
           </Box>
         </Container>
-      </AppProvider>
+      </Box>
+      <Container>
+        <Box className={classes.mainCheckout}>
+          <CheckoutForm existingCart={existingCart} />
+        </Box>
+      </Container>
     </ApolloProvider>
   );
 };
